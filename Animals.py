55904398b9e8fc18683263237641d0b0817a8dc9
MSmith <jobhuntr801@gmail.com>
2026-04-06 20:53:24 -0400

    Add files via upload

---
+++ Animals.py
@@ -0,0 +1,12 @@
+animals = ["dog", "cat", "rabbit"]
+
+for animal in animals:
+    print(animal)
+
+print()
+
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+
+print()
+print("Any of these animals would make a great pet!")
